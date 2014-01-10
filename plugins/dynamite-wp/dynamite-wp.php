94ff1b426a6980e022841aa625b4c83513ecf2f5
Damon Hastings <damonjhastings@gmail.com>
2014-01-10 03:54:58 -0800

    initial commit

---
+++ plugins/dynamite-wp/dynamite-wp.php
@@ -0,0 +1,9 @@
+<?php
+		/*
+		 * @package DynamiteWP
+		 * Plugin Name  : Dynamite WP - The Plugin
+		 * Description  : Provides a customizable framework to build out the various structural components of the website
+		 * Author		: ~== Damon
+		 * Version		: 0.1
+		 */
+?>
